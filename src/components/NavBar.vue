df9b557ef8d192cad9ad9c7a681524e9916bce4c
Hadeer Salah <Haduirsalah@gmail.com>
2025-08-23 15:27:05 +0300

    `Refactor project structure and dependencies`

---
+++ src/components/NavBar.vue
@@ -0,0 +1,73 @@
+<template>
+  <nav class="navbar">
+    <div class="logo">
+      <img src="@/assets/green-leaves-round-logo.png" :alt="logo" />
+    </div>
+    <div class="links">
+      <ul>
+        <li v-for="link in links" :key="link">
+          <a href="#">{{ link }}</a>
+        </li>
+      </ul>
+    </div>
+    <div class="btns">
+      <button>LOG IN</button>
+      <button>SIGN UP</button>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      logo: "Logo",
+      name: "NavBar",
+      links: ["Home", "About", "Contact", "Blog", "Careers"],
+    };
+  },
+};
+</script>
+<style>
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+  background-color: #333;
+  color: white;
+
+  .icon {
+    font-size: 24px;
+  }
+  .logo img {
+    height: 60px;
+  }
+  ul {
+    list-style: none;
+    display: flex;
+    gap: 20px;
+  }
+
+  a {
+    color: white;
+    text-decoration: none;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+  .btns {
+    button {
+      margin-left: 10px;
+      padding: 5px 10px;
+      border: none;
+      background-color: #42b983;
+      color: white;
+      cursor: pointer;
+      &:hover {
+        background-color: #369870;
+      }
+    }
+  }
+}
+</style>
