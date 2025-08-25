efae9bf3d2fc1594da1e1dee0eb6440bcf370135
Hadeer Salah <Haduirsalah@gmail.com>
2025-08-25 15:03:18 +0300

    31- Vue Cli (V-Model Part One ==> v-model and lazy mode)

---
+++ src/components/MyForm.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h2>My Form</h2>
+    <form @submit.prevent>
+      <div>
+        <label for="userName">userName:</label>
+        <input type="text" v-model.lazy="student.name" id="userName" required />
+        <p>userName is: {{ student.name }}</p>
+      </div>
+      <div>
+        <label for="lastName">lastName:</label>
+        <input
+          type="text"
+          v-model.lazy="student.lastName"
+          id="lastName"
+          required
+        />
+        <p>lastName is: {{ student.lastName }}</p>
+      </div>
+      <div>
+        <div>
+          <label for="age">Age:</label>
+          <input type="age" v-model.lazy="student.age" id="age" />
+          <p>Age is: {{ student.age }}</p>
+        </div>
+        <label for="email">Email:</label>
+        <input type="email" v-model.lazy="student.email" id="email" />
+        <p>Email is: {{ student.email }}</p>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyForm",
+  data() {
+    return {
+      student: {
+        userName: "",
+        lastName: "",
+        email: "",
+        age: null,
+      },
+    };
+  },
+
+  methods: {},
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,22 @@
 <template>
   <div>
     <h2>Home Page (View)</h2>
-    <p>Message from Child: {{ message }}</p>
+    <!-- <p>Message from Child: {{ message }}</p> -->
     <!-- استخدمنا الابن -->
-    <Child @childClicked="getMessageFromChild" />
-    <LifeCycle />
+    <!-- <Child @childClicked="getMessageFromChild" /> -->
+    <!-- <LifeCycle /> -->
+    <MyForm />
   </div>
 </template>
 
 <script>
-import Child from "../components/Child.vue";
-import LifeCycle from "../components/LifeCycle.vue";
+import MyForm from "../components/MyForm.vue";
 export default {
   name: "HomeView",
-  components: { Child, LifeCycle },
+  components: {
+    // Child, LifeCycle,
+    MyForm,
+  },
   data() {
     return {
       message: "",
